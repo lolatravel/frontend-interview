fcd74642a4acf7e2c23dd1585f3d4b0af26081f6
James Waters <jwaters627@gmail.com>
2019-10-03 13:58:43 -0400

    update to trip card

--- src/components/TripCard/ui.js
+++ src/components/TripCard/ui.js
@@ -5,18 +5,26 @@ import { TripType } from '../../constants';
 
 export class TripCardUI extends PureComponent {
     static propTypes = {
-        trip: PropTypes.arrayOf(TripType)
+        trip: PropTypes.arrayOf(TripType),
+        bookerName: PropTypes.string,
+        travelers: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string }))
     };
 
     static defaultProps = {
-        trip: null
+        trip: null,
+        bookerName: null,
+        travelers: []
     };
 
     render() {
-        const { trip } = this.props;
+        const { trip, bookerName, travelers } = this.props;
         return (
             <div className="tripCard">
+                <img className="tripImage" src={trip.image} alt="trip" />
                 <h3 className="nameText">{trip.name}</h3>
+                <text className="datesText">{trip.dates}</text>
+                <text className="uppercase">Booked By: {bookerName}</text>
+                <text className="uppercase">Travelers: {travelers}</text>
             </div>
         );
     }
